7cc42f8ab5702b178697284e94e9e17f3f49112f
AmzylMohcine <amzylmohcine@gmail.com>
2023-04-17 13:22:37 +0200

    Displaying platfm icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,12 +2,20 @@ import { useEffect, useState } from "react"
 import apiClient from "../services/api-client"
 import { CanceledError } from "axios"
 
+export interface Platform {
+  id: number
+  name: string
+  slug: string
+  background_image: string
+}
 export interface Game {
   id: number
   name: string
   rating: number
   background_image: string
+  parent_platforms: { platform: Platform }[]
 }
+
 interface FetchGames {
   count: number
   results: Game[]
